5b0116bf07a22476597c03b2e39f25080783e056
Madtanker <mr_tall_92@hotmail.com>
2017-12-09 16:49:20 -0800

    bouncer msg test

--- basicBot.js
+++ basicBot.js
@@ -246,7 +246,7 @@
     var botCreatorIDs = [3851534, 4105209, 3532061];
 
     var basicBot = {
-        version: '2.0.3',
+        version: '2.0.1',
         status: false,
         name: 'BasementCouch',
         loggedInID: null,
@@ -939,7 +939,7 @@
             if (botCreatorIDs.indexOf(user.id) > -1) {
               console.log(true);
                 API.sendChat('@'+user.username+' '+' has arrived!:fire::sparkles:');
-                if (basicBot.roomUtilities.rankToNumber === 2)
+            else if (basicBot.roomUtilities.rankToNumber === 2)
                 API.sendChat ( '@'+user.username+' '+' Everyone behave a bouncer is here!'); 
             } else if (basicBot.settings.welcome && greet) {
               console.log(false);
